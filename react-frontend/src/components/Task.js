0ca0d18d7ee37ed0f0582caf4dd6c1bbc826e1d6
Chik Lam <monicalamchik@gmail.com>
2021-06-11 22:23:52 -0400

    add color for each priority

--- react-frontend/src/components/Task.js
+++ react-frontend/src/components/Task.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { FaCheck } from 'react-icons/fa'
 
 import {ListGroup} from 'react-bootstrap'
-
+import { VscCircleFilled } from "react-icons/vsc";
 import ShowTaskContent from './ShowTaskContent'
 
 const Task = ({task,onDelete,deleteTask,editContent}) => {
+    const [color,setColor] = useState('');
+    useEffect(() => {
+        if (task.range === "1"){
+            setColor('blue');
+        }
+        else if (task.range === "2"){
+            setColor('green');
+        }
+        else if (task.range === "3"){
+            setColor("yellow")
+        }
+        else if (task.range === "4"){
+            setColor("orange")
+        }
+        else if (task.range === "5"){
+            setColor("red")
+        }
+        else{
+            setColor('blue')
+        }
+    }, [task.range])
     const [modalShow, setModalShow] = React.useState(false);
     return (
     <ListGroup variant="flush" >
         <ListGroup.Item action onDoubleClick={()=>setModalShow(true)}>
+        <VscCircleFilled style={{float:'left', marginRight:'1rem', color:color,fontSize:'1rem'}}/>
             {task.title} 
             <FaCheck onClick={()=>onDelete(task)}
             style={{float:'right', color:'green',cursor:'pointer',fontSize:'1.5rem'}}/>
