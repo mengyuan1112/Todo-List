c047d68ed995863921f19e44d41c0866ab976cc9
Chik Lam <monicalamchik@gmail.com>
2021-07-13 00:10:26 -0400

    added about us

--- react-frontend/src/components/AboutUs.js
+++ react-frontend/src/components/AboutUs.js
@@ -1,10 +1,57 @@
-
-
+import {Container,Row, Col,Image,Jumbotron} from 'react-bootstrap';
+import img from './../chiklam.JPG'
+import './AboutUs.css'
 const AboutUs = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+
+        <br/>
+        <br/>   
+        <Container className="justify-content-center">
+                <Row className="justify-content-center">
+                <Col>
+                    <br/>
+                    <br/>
+                        <p style={{fontSize:"2rem"}}>Chik Lam</p>
+                        <p>Fontend Software developer</p>
+                        <p><a href="https://github.com/lamchik1997">Github Link</a></p>
+                </Col>
+                <Col>
+                    <Image src={img} rounded style={{width:'34%'}} />
+                </Col>
+                </Row>
+        <hr/>
+        <br/>
+        <Row className="justify-content-center">
+                <Col>
+                    <br/>
+                    <br/>
+                        <p style={{fontSize:"2rem"}}>Name here </p>
+                        <p>Role here</p>
+                        <p><a href="XXX">Github Link</a></p>
+                </Col>
+                <Col>
+                    <Image src="xxx" rounded style={{width:'34%'}} />
+                </Col>
+                </Row>
+
+                <hr/>
+        <br/>
+        <Row className="justify-content-center">
+                <Col>
+                    <br/>
+                    <br/>
+                        <p style={{fontSize:"2rem"}}>Name here </p>
+                        <p>Role here</p>
+                        <p><a href="XXX">Github Link</a></p>
+                </Col>
+                <Col>
+                    <Image src="xxx" rounded style={{width:'34%'}} />
+                </Col>
+                </Row>
+        </Container>
+        
+        </>       
     )
 }
 
