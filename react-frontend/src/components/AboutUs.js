169509b5110ec83e52a75239cceb7c70976b8747
mengyuan1112 <yuanmeng@buffalo.edu>
2021-07-13 08:32:47 -0400

    update

--- react-frontend/src/components/AboutUs.js
+++ react-frontend/src/components/AboutUs.js
@@ -14,8 +14,9 @@ const AboutUs = () => {
                     <br/>
                     <br/>
                         <p style={{fontSize:"2rem"}}>Chik Lam</p>
-                        <p>Fontend Software developer</p>
+                        <p>Frontend Software developer</p>
                         <p><a href="https://github.com/lamchik1997">Github Link</a></p>
+                        <p>Contact Email: ...</p>
                 </Col>
                 <Col>
                     <Image src={img} rounded style={{width:'34%'}} />
@@ -30,6 +31,7 @@ const AboutUs = () => {
                         <p style={{fontSize:"2rem"}}>Yuan Meng</p>
                         <p>Backend Developer</p>
                         <p><a href="https://github.com/mengyuan1112">Github Link</a></p>
+                        <p>Contact Email: yuanmeng@buffalo.edu</p>
                 </Col>
                 <Col>
                     <Image src={yuanImg} rounded style={{width:'34%'}} />
@@ -45,6 +47,7 @@ const AboutUs = () => {
                         <p style={{fontSize:"2rem"}}>Name here </p>
                         <p>Role here</p>
                         <p><a href="XXX">Github Link</a></p>
+                        <p>Contact Email: ...</p>
                 </Col>
                 <Col>
                     <Image src="xxx" rounded style={{width:'34%'}} />
